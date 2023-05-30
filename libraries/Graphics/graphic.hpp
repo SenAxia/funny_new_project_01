2cc5ba476dcfa9c63e720979c1f1292c3189161a
senaxia <precieux.dagba@epitech.eu>
2023-05-30 12:05:47 +0100

    Display windows, dots position get, sand draw on screen

--- libraries/Graphics/graphic.hpp
+++ libraries/Graphics/graphic.hpp
@@ -13,32 +13,58 @@
     using namespace sf;
 
     class draw {
+        public:
+        RectangleShape dots;
 
         draw () {}
         ~draw () {}
+
+        void drawPixels(vector<Vector2i> coords, RenderWindow window) {
+            for (auto pixels_coords : coords) {
+                dots.setSize({1, 1});
+                dots.setPosition(Vector2f(pixels_coords.x, pixels_coords.y));
+                dots.setFillColor(Color::Red);
+                window.draw(dots);
+            }
+        }
     };
 
     class graphic {
         public:
         RenderWindow window = RenderWindow(VideoMode(1000,800),"Sander");
+        // vector<Vector2i> coordPoses;
+        draw pixels;
+        RectangleShape dots;
 
         graphic () {}
         ~graphic () {}
+
+        void drawPixels(vector<Vector2i> coords, RenderWindow& window) {
+            for (auto pixels_coords : coords) {
+                dots.setSize({1, 1});
+                dots.setPosition(Vector2f(pixels_coords.x, pixels_coords.y));
+                dots.setFillColor(Color::Red);
+                window.draw(dots);
+            }
+        }
 
         void initLoop() {
             while (window.isOpen()) {
                 Event e;
                 while (window.pollEvent(e)) {
                     if (Mouse::isButtonPressed(Mouse::Left)) {
-                        Vector2i pix_pos = Mouse::getPosition();
-                        //window.close();
+                        Vector2i coordPoses = Mouse::getPosition(window);
+                        dots.setSize({1, 1});
+                        dots.setPosition(Vector2f(coordPoses.x, coordPoses.y));
+                        dots.setFillColor(Color::Red);
+                        window.draw(dots);
                     }
                     if (e.type == Event::Closed)
                         window.close();
                 }
+                window.display();
             }
             window.clear();
-            window.display();
         }
     };
 
